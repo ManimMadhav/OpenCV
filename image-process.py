3de354864f6fd9e245741e99a5474e3754696a36
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2021-07-21 13:04:29 +0530

    Create image-process.py

---
+++ image-process.py
@@ -0,0 +1,7 @@
+#image processing with opencv
+import cv2
+img = cv2.imread('image.jfif',1)
+
+"""each pixel is denoted by 3 values
+   RGB (which is standard),
+   but in OpenCV, the standard pixel ir denoted in a BGR form"""
